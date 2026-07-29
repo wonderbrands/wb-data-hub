80d3eb109f4c0618d593f59994b5870a65d4adf9
Sergio Gil <sergio@wonderbrands.co>
2026-07-29 15:36:09 -0600

    [UPD] anticollition general invoice Odoo

--- processes/wonderbrands/financial/billing_processes/odoo/facturacion_automatica_1a1.py
+++ processes/wonderbrands/financial/billing_processes/odoo/facturacion_automatica_1a1.py
@@ -75,7 +75,7 @@ PARTNER_ID_PUBLICO_GENERAL = 13436
 
 # =======================================================================
 # CONFIGURACION DE PRUEBAS
-TEST_ORDER_LIMIT = 5  # None -> historico.
+TEST_ORDER_LIMIT = 100  # None -> historico.
 # =======================================================================
 
 # --- TAMANOS DE LOTE (controlan el techo de memoria) ---
